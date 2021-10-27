0505320fa696436ed9e155ae655316e1c622b631
administrator <administrator@49293.local>
2021-10-27 23:02:21 +0100

    create register and sign in interfaces

--- Messenger/Controller/Authentication/LogInViewController.swift
+++ Messenger/Controller/Authentication/LogInViewController.swift
@@ -12,6 +12,8 @@ class LogInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "space background.png")!)
+
     }
 
 
